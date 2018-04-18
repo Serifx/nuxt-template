f5a47e0fb6c6db1e788462919382e89456443832
Serifx <Serifx@outlook.com>
2018-04-18 17:00:32 +0800

    # update

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,9 @@ module.exports = {
   loading: { color: '#3B8070' },
 
   build: {
+    cache: false,
+    extractCSS: true,
+
     publicPath: dirPublic,
 
     filenames: {
@@ -44,10 +47,32 @@ module.exports = {
       css: dirStyle + '/[name].[contenthash].css'
     },
 
-    extractCSS: true,
-
     vendor: [],
-    cache: false,
+
+    // https://www.w3cplus.com/mobile/vw-layout-in-vue.html
+    postcss: [
+      require('postcss-import')(),
+      require('postcss-url')(),
+      require('postcss-aspect-ratio-mini')(),
+      require('postcss-write-svg')(),
+      require('postcss-cssnext')(),
+      require('postcss-px-to-viewport')({
+        viewportWidth: 750,   // (Number) The width of the viewport.
+        viewportHeight: 1334, // (Number) The height of the viewport.
+        unitPrecision: 5,     // (Number) The decimal numbers to allow the REM units to grow to.
+        viewportUnit: 'vw',   // (String) Expected units.
+        selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave
+                                                      // as px.
+        minPixelValue: 1,     // (Number) Set the minimum pixel value to replace.
+        mediaQuery: false     // (Boolean) Allow px to be converted in media queries.
+      }),
+      require('postcss-viewport-units')(),
+      require('cssnano')({
+        preset: 'advanced',
+        autoprefixer: false,
+        'postcss-zindex': false
+      })
+    ],
 
     loaders: [
       {
@@ -101,8 +126,8 @@ module.exports = {
     ],
 
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
